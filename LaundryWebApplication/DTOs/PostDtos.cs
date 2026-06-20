b98dfd28fb6712b00a4f6a2a5154ba8fa3e1e16c
MarcelKrawczyk <marcelkrawczyk04@gmail.com>
2026-06-21 01:32:43 +0200

    Implements POST mehtod DTOs

--- LaundryWebApplication/DTOs/PostDtos.cs
+++ LaundryWebApplication/DTOs/PostDtos.cs
@@ -1,6 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LaundryWebApplication.DTOs;
 
-public class PostDtos
+public class PostRequestDTO
 {
+    [Required] public WashingMachineDTO WashingMachine { get; set; } = null!;
     
+    [Required]
+    public List<PostAvailableProgramsDTO> availablePrograms { get; set; } = new();
+}
+public class PostAvailableProgramsDTO
+{
+    [Required]
+    [MaxLength(50)]
+    public string ProgramName { get; set; } = null!;
+    
+    [Range(0, 25)]
+    public decimal Price { get; set; }
+}
+
+public class WashingMachineDTO
+{
+    [Range(8, double.MaxValue)] 
+    public decimal MaxWeight { get; set; }
+
+    [MaxLength(100)] 
+    public string SerialNumber { get; set; } = null!;
 }
